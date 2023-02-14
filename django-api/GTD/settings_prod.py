b7baec55ebebf780305d9231f408267db1351937
Pedro Brasil <ppbrasil@gmail.com>
2023-02-14 13:35:23 -0300

    and another...

--- django-api/GTD/settings_prod.py
+++ django-api/GTD/settings_prod.py
@@ -14,7 +14,7 @@ DATABASES = {
         'NAME': os.environ.get('gtd-mysql-1'),
         'USER': os.environ.get('admin'),
         'PASSWORD': os.environ.get('Db_Pass!'),
-        'HOST': os.environ.get('mysql -h gtd-mysql-1.cghnoav6qten.us-east-1.rds.amazonaws.com'),
+        'HOST': os.environ.get('gtd-mysql-1.cghnoav6qten.us-east-1.rds.amazonaws.com'),
         'PORT': os.environ.get('3306'),
     }
 }
