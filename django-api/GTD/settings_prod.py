5bd3a342ceb656be662867eecd69630fc907b387
Pedro Brasil <ppbrasil@gmail.com>
2023-02-14 13:18:07 -0300

    adjust prod settings

--- django-api/GTD/settings_prod.py
+++ django-api/GTD/settings_prod.py
@@ -11,10 +11,10 @@ ALLOWED_HOSTS = [
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('django.db.backends.mysql'),
-        'NAME': os.environ.get('django_db'),
-        'USER': os.environ.get('django_user'),
-        'PASSWORD': os.environ.get('secret'),
-        'HOST': os.environ.get('db'),
+        'NAME': os.environ.get('gtd-mysql-1'),
+        'USER': os.environ.get('admin'),
+        'PASSWORD': os.environ.get('Db_Pass!'),
+        'HOST': os.environ.get('mysql -h gtd-mysql-1.cghnoav6qten.us-east-1.rds.amazonaws.com'),
         'PORT': os.environ.get('3306'),
     }
 }
