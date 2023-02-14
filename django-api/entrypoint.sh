e1424edc8604a6dc92461f29b9c301e9f2fb5268
Pedro Brasil <ppbrasil@gmail.com>
2023-02-14 19:19:08 -0300

    qwer

--- django-api/entrypoint.sh
+++ django-api/entrypoint.sh
@@ -7,4 +7,4 @@ while ! nc -z gtd-mysql-1.cghnoav6qten.us-east-1.rds.amazonaws.com 3306; do
 done
 
 # Start the Django development server
-exec python3 manage.py runserver
+exec python3 manage.py runserver 0.0.0.0:8000
