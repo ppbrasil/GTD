5f3a3309b58487d5dae7b8dfc79b46731a39171b
Pedro Brasil <ppbrasil@gmail.com>
2023-02-14 18:50:39 -0300

    nonon

--- django-api/entrypoint.sh
+++ django-api/entrypoint.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-# Set the host based on the value of ENV
-if [ "$ENV" == "dev" ]; then
-  HOST="bd"
-elif [ "$ENV" == "prod" ]; then
-  HOST="gtd-mysql-1.cghnoav6qten.us-east-1.rds.amazonaws.com"
-else
-  echo "Invalid value for ENV: $ENV"
-  exit 1
-fi
-
 # Wait for the database service to be ready
-while ! nc -z "$HOST" 3306; do
-  echo "Waiting for database service on $HOST..."
+while ! nc -z gtd-mysql-1.cghnoav6qten.us-east-1.rds.amazonaws.com 3306; do
+  echo "Waiting for database service on gtd-mysql-1.cghnoav6qten.us-east-1.rds.amazonaws.com 3306..."
   sleep 1
 done
 
 # Start the Django development server
-exec python3 manage.py runserver "$HOST:8000"
+exec python3 manage.py runserver
