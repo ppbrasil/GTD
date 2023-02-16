901db616671fac5b9a2119cbcbd5923bcf88af50
Pedro Brasil <ppbrasil@gmail.com>
2023-02-16 08:25:44 -0300

    Add cron to container + the update_overdue routine

--- django-api/entrypoint.sh
+++ django-api/entrypoint.sh
@@ -13,7 +13,7 @@ fi
 
 # Wait for the database service to be ready
 while ! nc -z "$HOST" 3306; do
-  echo "Waiting for database service on gtd-mysql-1.cghnoav6qten.us-east-1.rds.amazonaws.com 3306..."
+  echo "Waiting for database service on $HOST 3306..."
   sleep 1
 done
 
