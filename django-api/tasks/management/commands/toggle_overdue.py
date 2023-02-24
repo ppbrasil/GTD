35756d6563dece19fe27f14c015f74dc12b90a22
Pedro Brasil <ppbrasil@gmail.com>
2023-02-24 08:45:59 -0300

    Implement cron + script to overdue and today focus

---
+++ django-api/tasks/management/commands/toggle_overdue.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from tasks.models import Task
+
+
+class Command(BaseCommand):
+    help = 'Toggle overdue for overdue tasks'
+
+    def handle(self, *args, **options):
+        now = timezone.now().date()
+        overdue_tasks = Task.objects.filter(due_date__lte=now, overdue=False, is_active=True)
+        overdue_tasks.update(overdue=True)
